f25c166061aa96ab32950f719caa16a9f4d35154
Johnny Gouveia <johnny.gouveia@sonner.com.br>
2024-11-10 17:36:11 -0300

    Atualização na etapa 3 - Número de corte agora vem aleatório de acordo com o length do número binário;
    E agora se um número for maior que o outro, o código adiciona os "0" correspondentes;

--- etapa3.java
+++ etapa3.java
@@ -1,18 +1,42 @@
+import java.util.Random;
+
 public class etapa3 {
 
     public static void main(String[] args) {
 
-        cruzamentoBinarios("11010101", "10111011", 4);
+        cruzamentoBinarios("11010101", "10111011");
 
     }
 
-    public static void cruzamentoBinarios(String binario1, String binario2, int numeroCorte) {
+    public static void cruzamentoBinarios(String binario1, String binario2) {
+        Random random = new Random();
+
+        int tamanhoDoBinario1 = binario1.length();
+        int tamanhoDoBinario2 = binario2.length();
+
+        if (tamanhoDoBinario1 > tamanhoDoBinario2) {
+            int diferenca = tamanhoDoBinario1 - tamanhoDoBinario2;
+
+            for (int i = 0; i < diferenca; i++) {
+                System.out.print(i);
+                binario2 = "0" + binario2;
+            }
+
+        } else if (tamanhoDoBinario2 > tamanhoDoBinario1) {
+            int diferenca = tamanhoDoBinario2 - tamanhoDoBinario1;
+
+            for (int i = 0; i < diferenca; i++) {
+                System.out.print(i);
+                binario1 = "0" + binario1;
+            }
+        }
+
+        int numeroCorte = random.nextInt(tamanhoDoBinario1);
+
         String[] listaB1 = binario1.split("");
         String binario1Parte1 = "";
-        String binario1Parte2 = "";
 
         String[] listaB2 = binario2.split("");
-        String binario2Parte1 = "";
         String binario2Parte2 = "";
 
         for (int i = 0; i < listaB1.length; i++) {
@@ -30,8 +54,9 @@ public class etapa3 {
         System.out.println();
         System.out.println("============ Binários cruzados ============");
         System.out.println();
+        System.out.println("Seu corte ficou na casa: " + numeroCorte);
+        System.out.println();
         System.out.println(binario1Parte1 + " " + binario2Parte2);
-        System.out.println(binario2Parte1 + " " + binario1Parte2);
         System.out.println();
         System.out.println("===========================================");
         System.out.println();
